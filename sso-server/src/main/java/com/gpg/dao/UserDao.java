d6f4a7c954ff293b60a9ea04c000f81a079e7d49
Gzpin <1609328059@qq.com>
2018-08-13 09:20:23 +0800

    user find by name and password

--- sso-server/src/main/java/com/gpg/dao/UserDao.java
+++ sso-server/src/main/java/com/gpg/dao/UserDao.java
@@ -5,4 +5,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserDao extends BaseDao<User> {
+
+    User findByUserNameAndUserPass(String userName,String userPass);
+
 }
